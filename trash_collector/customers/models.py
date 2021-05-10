6ef4fc7daa1b4585ea75efd67cc4b81a5a84f2f3
hkl-iii <rahleggon@gmail.com>
2021-05-10 14:19:04 -0500

    "migrated required files"

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,9 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    pickup_date = models.CharField(max_length=50)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    balance = models.IntegerField(default=0)
+    zipcode = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
